12827751fd996b7dde237be4ef0e0025fe059d16
MoRashad <mohamed.rashad@msa.edu.eg>
2022-12-05 16:50:14 +0200

    login and registration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React from "react";
 import "./App.css";
-import HomeScreen from "./HomeScreen";
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { auth } from "./firebase";
 
 function App() {
+  const user = null;
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAth) => {
+      if (userAth) {
+        // logged in
+        console.log(userAth);
+      } else {
+        // logged out
+      }
+    });
+
+    return unsubscribe;
+  }, []);
   return (
     <div className="app">
-      <HomeScreen />
+      <Router>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Switch>
+            <Route exact path="/">
+              <HomeScreen />
+            </Route>
+          </Switch>
+        )}
+      </Router>
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,8 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,4 +17,9 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
